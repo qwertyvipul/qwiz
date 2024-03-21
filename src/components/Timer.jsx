23a1d06f01c4bb718e6c8a566553ef46c7d4996a
/ | | |_| |_ <vipuls526@gmail.com>
2024-03-21 16:15:58 +0530

    Update Timer.jsx

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,8 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { getTimeString } from "../utils/time";
 
-function Timer() {
-  useEffect(() => {}, []);
-  return <div>Timer</div>;
+function Timer({ onTick }) {
+  const [seconds, setSeconds] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(
+      () =>
+        setSeconds((prev) => {
+          const next = prev + 1;
+          onTick(next);
+          return next;
+        }),
+      1000
+    );
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+  return <span className="badge secondary">{getTimeString(seconds)}</span>;
 }
 
 export default React.memo(Timer);
